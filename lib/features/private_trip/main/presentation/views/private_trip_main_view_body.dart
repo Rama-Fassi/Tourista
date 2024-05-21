173688e08c0bf03dc6c1041ac23aae819fb9313f
raghadhawat <hawatraghad123@gmail.com>
2024-05-21 22:41:50 +0300

    date and person number done in private trip main

--- lib/features/private_trip/main/presentation/views/private_trip_main_view_body.dart
+++ lib/features/private_trip/main/presentation/views/private_trip_main_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -9,10 +10,20 @@ import 'package:tourista/core/utlis/styles.dart';
 import 'package:tourista/core/widgets/custom_button.dart';
 import 'package:tourista/features/auth/sign_in_and_up/presentation/views/widgets/earth_logo_with_text.dart';
 import 'package:tourista/features/private_trip/main/presentation/views/widgets/plan_table.dart';
+import 'package:tourista/features/private_trip/main/presentation/views/widgets/select_number_widget.dart';
 import 'package:tourista/features/private_trip/main/presentation/views/widgets/table_row_widget.dart';
 
-class PrivatTripMainViewBody extends StatelessWidget {
-  const PrivatTripMainViewBody({super.key});
+class PrivatTripMainViewBody extends StatefulWidget {
+  const PrivatTripMainViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<PrivatTripMainViewBody> createState() => _PrivatTripMainViewBodyState();
+}
+
+class _PrivatTripMainViewBodyState extends State<PrivatTripMainViewBody> {
+  DateTime? startDate;
+  DateTime? endDate;
+  int selectedNumber = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -28,26 +39,35 @@ class PrivatTripMainViewBody extends StatelessWidget {
         image: Assets.imagesIconsEnterDestination,
       ),
       TableRowWidget(
-        onTap: () {},
+        onTap: () async {
+          var results = await createCalendar(context);
+          checkOnDate(results, startDate, endDate);
+        },
         text: LocaleKeys.selectDates.tr(),
         image: Assets.imagesIconsSelectDates,
       ),
       TableRowWidget(
-        onTap: () {},
+        onTap: () {
+          showPersonNumberBottomSheet(context);
+        },
         text: LocaleKeys.enterPersonNumber.tr(),
       ),
       CustomButton(
-          onTap: () {
-            GoRouter.of(context).push(AppRouter.kPrivateTripTapBar);
-          },
-          text: LocaleKeys.continueButton.tr(),
-          width: MediaQuery.sizeOf(context).width,
-          borderRadius: 10,
-          height: MediaQuery.sizeOf(context).height * .08,
-          style: AppStyles.styleInterBold20(context)
-              .copyWith(color: Colors.white, fontSize: 21),
-          color: kPrimaryColor),
+        onTap: () {
+          GoRouter.of(context).push(AppRouter.kPrivateTripTapBar);
+          // Access the selected number here
+          print('Selected number: $selectedNumber');
+        },
+        text: LocaleKeys.continueButton.tr(),
+        width: MediaQuery.of(context).size.width,
+        borderRadius: 10,
+        height: MediaQuery.of(context).size.height * .08,
+        style: AppStyles.styleInterBold20(context)
+            .copyWith(color: Colors.white, fontSize: 21),
+        color: kPrimaryColor,
+      ),
     ];
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
@@ -58,10 +78,71 @@ class PrivatTripMainViewBody extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 15),
           child: PlanTable(tableList: tableList),
         ),
-        const Spacer(
-          flex: 4,
-        )
+        const Spacer(flex: 4),
       ],
+    );
+  }
+
+  void checkOnDate(
+      List<DateTime?>? results, DateTime? startDate, DateTime? endDate) {
+    if (results != null && results.isNotEmpty) {
+      // Access the selected value from the results variable
+      startDate = results[0];
+      endDate = results[1];
+
+      // Do something with the selected dates
+      print("Selected start date: $startDate");
+      print("Selected end date: $endDate");
+    } else {
+      // Handle the case when no date is selected
+      print("No date selected");
+    }
+  }
+
+  Future<List<DateTime?>?> createCalendar(BuildContext context) {
+    return showCalendarDatePicker2Dialog(
+      context: context,
+      config: CalendarDatePicker2WithActionButtonsConfig(
+        calendarType: CalendarDatePicker2Type.range,
+      ),
+      dialogSize: const Size(325, 400),
+      value: [],
+      borderRadius: BorderRadius.circular(15),
+    );
+  }
+
+  void showPersonNumberBottomSheet(BuildContext context) {
+    showModalBottomSheet<void>(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return FractionallySizedBox(
+          widthFactor: 1.0,
+          heightFactor: 0.2,
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    LocaleKeys.enterPersonNumber.tr(),
+                    style: AppStyles.styleInterSemiBold18(context),
+                  ),
+                  SelectNumberWidget(
+                    onNumberChanged: (int number) {
+                      setState(() {
+                        selectedNumber = number;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
