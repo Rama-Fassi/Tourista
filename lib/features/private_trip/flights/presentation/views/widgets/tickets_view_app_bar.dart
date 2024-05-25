e668ad69357f20a5eb0ff98b9305342406f3c350
raghadhawat <hawatraghad123@gmail.com>
2024-05-25 08:59:15 +0300

    tickets part one

--- lib/features/private_trip/flights/presentation/views/widgets/tickets_view_app_bar.dart
+++ lib/features/private_trip/flights/presentation/views/widgets/tickets_view_app_bar.dart
@@ -1,8 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tourista/constants.dart';
+import 'package:tourista/core/translations/locale_keys.g.dart';
 import 'package:tourista/core/utlis/app_assets.dart';
 import 'package:tourista/core/utlis/styles.dart';
 
@@ -21,13 +23,36 @@ class TickesViewAppBar extends StatelessWidget {
     return Stack(
       clipBehavior: Clip.none,
       children: [
-        Container(
-          width: width,
-          height: height * .09,
-          color: kPrimaryColor,
+        Column(
+          children: [
+            Container(
+              width: width,
+              height: height * .09,
+              color: kPrimaryColor,
+            ),
+            Container(
+              width: width,
+              decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(.1),
+                  blurRadius: 1,
+                  offset: const Offset(0, 4),
+                )
+              ]),
+              child: Padding(
+                padding:
+                    EdgeInsets.only(top: height * .035, bottom: 10, left: 16),
+                child: Text(
+                  '5  ${LocaleKeys.flightsFound.tr()}',
+                  style: AppStyles.styleSourceSemiBold20(context)
+                      .copyWith(color: Colors.black.withOpacity(.8)),
+                ),
+              ),
+            ),
+          ],
         ),
         Positioned(
-            top: height * .05,
+            top: height * .04,
             left: width * .12,
             child: Container(
               decoration: BoxDecoration(
@@ -61,10 +86,10 @@ class TickesViewAppBar extends StatelessWidget {
                     ' 25-5 ',
                     style: AppStyles.styleInterMedium18(context),
                   ),
-                  Gap(10),
+                  Gap(25),
                 ],
               ),
-            ))
+            )),
       ],
     );
   }
--- lib/features/private_trip/flights/presentation/views/widgets/tickets_view_body.dart
+++ lib/features/private_trip/flights/presentation/views/widgets/tickets_view_body.dart
@@ -1,4 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:tourista/core/translations/locale_keys.g.dart';
+import 'package:tourista/core/utlis/styles.dart';
+import 'package:tourista/features/private_trip/flights/presentation/views/widgets/sort_by_tickets_row.dart';
+import 'package:tourista/features/private_trip/flights/presentation/views/widgets/tickets_time.dart';
 import 'package:tourista/features/private_trip/flights/presentation/views/widgets/tickets_view_app_bar.dart';
 
 class TicketsViewBody extends StatelessWidget {
@@ -11,7 +16,41 @@ class TicketsViewBody extends StatelessWidget {
     return Column(
       children: [
         TickesViewAppBar(width: width, height: height),
+        Padding(
+          padding: const EdgeInsets.all(12),
+          child: Text(
+            LocaleKeys.priceMyChange.tr(),
+            style: AppStyles.styleInterMedium18(context)
+                .copyWith(color: Colors.black.withOpacity(.5)),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(16),
+          width: width,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [boxShadow(4, 4), boxShadow(-4, 4)]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SortByTicketsRow(),
+              TicketsTime(
+                time: '17:5',
+                airport: "DXB",
+                date: '18-5',
+              )
+            ],
+          ),
+        )
       ],
+    );
+  }
+
+  BoxShadow boxShadow(double y, double blur) {
+    return BoxShadow(
+      color: Colors.grey.withOpacity(.1),
+      blurRadius: blur,
+      offset: Offset(0, y),
     );
   }
 }
